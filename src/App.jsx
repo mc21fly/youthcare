9697a5a01269b7111aca454c257cf5ddc99a4e03
Michal <michalbkl@poczta.fm>
2024-08-07 18:15:02 +0200

    config changed

--- src/App.jsx
+++ src/App.jsx
@@ -15,10 +15,15 @@ export default function App() {
         setCurrentView("view4");
     }
 
+    function changeView(view) {
+        window.scrollTo(0, 0);
+        setCurrentView(view);
+    }
+
     function displayView() {
-        if (currentView === "view1") return <View1 handleNext={() => setCurrentView("view2")} />;
-        if (currentView === "view2") return <View2 handleNext={() => setCurrentView("view3")} handlePrev={() => setCurrentView("view1")} />;
-        if (currentView === "view3") return <View3 handleNext={send} handlePrev={() => setCurrentView("view2")} />;
+        if (currentView === "view1") return <View1 handleNext={() => changeView("view2")} />;
+        if (currentView === "view2") return <View2 handleNext={() => changeView("view3")} handlePrev={() => changeView("view1")} />;
+        if (currentView === "view3") return <View3 handleNext={send} handlePrev={() => changeView("view2")} />;
         if (currentView === "view4") return <View4 />;
     }
 
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
+import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react(), viteSingleFile()],
+    plugins: [
+        react(),
+        legacy({
+            targets: ["defaults", "not IE 11"],
+        }),
+        viteSingleFile(),
+    ],
     build: {
         minify: true,
     },
