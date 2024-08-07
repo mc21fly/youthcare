959536c852e3a47381af171ec82e3762819a352c
Michal <michalbkl@poczta.fm>
2024-08-07 18:18:38 +0200

    config changed

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { viteSingleFile } from "vite-plugin-singlefile";
-import legacy from "@vitejs/plugin-legacy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        react(),
-        legacy({
-            targets: ["defaults", "not IE 11"],
-        }),
-        viteSingleFile(),
-    ],
+    plugins: [react(), viteSingleFile()],
     build: {
         minify: true,
     },
