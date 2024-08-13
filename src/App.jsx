e1e35044f98acdd29cccb13d6e7c9e20d8b64ecf
Michal Blakala <michal.blakala@performanceart.com>
2024-08-13 15:55:54 +0200

    notification added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Header, Footer } from "./components";
 import { View1, View2, View3, View4 } from "./views";
 import { useStorage } from "./hooks";
 import translate from "./helpers/translate";
+import Notify from "simple-notify";
+import "simple-notify/dist/simple-notify.css";
 
 export default function App() {
     const [currentView, setCurrentView] = useState("view1");
@@ -24,6 +26,29 @@ export default function App() {
             changeView("view4");
             localStorage.clear();
         }
+
+        if (json && json.status === 400) {
+            new Notify({
+                status: "error",
+                title: "Something went wrong",
+                text: "Please fill out all required fields",
+                effect: "fade",
+                speed: 300,
+                customClass: "",
+                customIcon: "",
+                showIcon: true,
+                showCloseButton: true,
+                autoclose: true,
+                autotimeout: 5000,
+                notificationsGap: null,
+                notificationsPadding: null,
+                type: "outline",
+                position: "right bottom",
+                customWrapper: "",
+            });
+        }
+
+        console.log(json);
 
         setSending(false);
     }
